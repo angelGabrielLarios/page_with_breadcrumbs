c76fcb9558372dc13ca6a218cafb202cc1733659
Angel Larios <lariosacostaa@gmail.com>
2024-02-05 20:27:09 -0600

    ya me asegure que el comando npm run build si saliera

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,11 @@
 
 import { Outlet } from "react-router-dom"
 import { Navbar } from "../components"
-import { useHomePage } from "../hooks"
+
 
 
 export const HomePage = () => {
 
-    useHomePage()
 
 
 
